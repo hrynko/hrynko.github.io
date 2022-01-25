5b21bfcaa8a4ca58f942614f7ee7289272602ba2
Aliaksei Hrynko <aliaksei.hrynko@gmail.com>
2022-01-25 19:47:50 +0300

    Core: Add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,15 @@
+<template>
+  <Layout>
+    <h1>Page not found</h1>
+
+    <p>The page doesn't exist or was removed.</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Page not found',
+  },
+}
+</script>
