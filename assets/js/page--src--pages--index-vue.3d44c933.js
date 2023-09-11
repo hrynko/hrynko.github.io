fd90282e450843a47abad4749868a1b6b0c40a58
Aliaksei Hrynko <aliaksei.hrynko@gmail.com>
2023-09-11 21:29:26 +0200

    Updates

---
+++ assets/js/page--src--pages--index-vue.3d44c933.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{iyQ6:function(e,t,n){"use strict";n.r(t);var l={metaInfo:{title:"Index"},mounted(){this.$router.replace("/blog/")}},i=n("KHd+"),o=Object(i.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);t.default=o.exports}}]);
